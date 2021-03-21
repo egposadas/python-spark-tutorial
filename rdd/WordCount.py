7cb419c0ef23de766a7d05a5c3540af2ee651ec4
egposadas <posadas.lalo@gmail.com>
2021-03-20 20:45:47 -0600

    test

--- rdd/WordCount.py
+++ rdd/WordCount.py
@@ -3,8 +3,8 @@ from pyspark import SparkContext, SparkConf
 if __name__ == "__main__":
     conf = SparkConf().setAppName("word count").setMaster("local[3]")
     sc = SparkContext(conf = conf)
-    
-    lines = sc.textFile("in/word_count.text")
+
+    lines = sc.textFile("../in/word_count.text")
     
     words = lines.flatMap(lambda line: line.split(" "))
     
